033819af75b892ed649db51f746610511048d6d6
Saburollah Safari <Saburollahsafari@gmail.com>
2026-08-14 22:14:34 +0200

    temporarily enable exception details

--- backend/src/Api/Program.cs
+++ backend/src/Api/Program.cs
@@ -170,8 +170,6 @@ builder.Services.AddSwaggerGen(options =>
 
 var app = builder.Build();
 
-app.UseDeveloperExceptionPage();
-
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
